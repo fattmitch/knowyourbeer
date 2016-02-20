2f244ec4935da596ca0cd7262d21b27a63a4a120
fattmitch <matt.s.fitch@gmail.com>
2016-02-19 16:24:22 -0800

    Angularified

---
+++ app/scripts/app.js
@@ -0,0 +1,31 @@
+(function() {
+    function config($stateProvider, $locationProvider) {
+        $locationProvider
+            .html5Mode({
+                enabled: true,
+                requireBase: false
+        });
+        
+        $stateProvider
+            .state('landing', {
+                url: '/',
+                templateUrl: '/templates/landing.html'
+        })
+        
+        $stateProvider
+            .state('search-results', {
+                url: '/search-results',
+                templateUrl: '/templates/search-results.html'    
+        })
+        
+        $stateProvider
+            .state('beer-detail', {
+                url: '/beer-details',
+                templateUrl: '/templates/beer-detail.html'
+        });
+    }
+    
+    angular
+        .module('knowYourBeer', ['ui.router'])
+        .config(config);
+ })();
