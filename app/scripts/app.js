00298249667fa6d5c5588816e035b6c6cd412e5b
fattmitch <matt.s.fitch@gmail.com>
2016-03-10 16:40:39 -0800

    added filter and contact us page.

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -10,19 +10,19 @@
             .state('landing', {
                 url: '/',
                 templateUrl: '/templates/landing.html'
-        })
+            })
         
             .state('search-results', {
                 url: '/search-results/:query',
                 controller: 'SearchCtrl as search',
                 templateUrl: '/templates/search-results.html'
-        })
+            })
         
             .state('beer-detail', {
                 url: '/beer-detail/:id',
                 controller: 'BeerDetailCtrl as beerDetail',
                 templateUrl: '/templates/beer-detail.html'
-        })
+            })
         
             .state('brewery-detail', {
                     url: '/brewery-detail/:id',
@@ -33,6 +33,11 @@
             .state('about-us', {
                 url: '/about',
                 templateUrl: '/templates/about-us.html'
+            })
+        
+            .state('contact-us', {
+                url: '/contact',
+                templateUrl: '/templates/contact-us.html'
             });
     }
     
